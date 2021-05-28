ebad4254edcddb605cdf52705c8d26b0fbbd2a9c
jackhwolf <jwolf22@wisc.edu>
2021-05-28 17:14:56 -0500

    work

--- tmux_deploy.py
+++ tmux_deploy.py
@@ -12,8 +12,10 @@ class Deployer:
         cmd = f"python3 {fname} {workers}"
         pane =  self.session.attached_window.attached_pane
         pane.send_keys('source venv/bin/activate')
-        pane.send_keys('export OMP_NUM_THREADS=1')
-        # pane.send_keys('export OMP_NUM_THREADS=12')
+        keys = ['OMP_NUM_THREADS', 'MKL_NUM_THREADS', 'NUMEXPR_NUM_THREADS', \
+                'VECLIB_MAXIMUM_THREADS', 'OPENBLAS_NUM_THREADS']
+        for key in keys:
+            pane.send_keys(f'export {key}=1')
         pane.send_keys(cmd)
 
 if __name__ == '__main__':
